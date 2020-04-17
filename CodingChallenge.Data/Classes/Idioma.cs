3cffd82fc48baaf217bd995e6ab8f701138490f7
Diego Mariano Verrastro <diegoxtr@gmail.com>
2020-04-16 23:53:12 -0300

    #06 changed virtual class for abstract in Idiom.

--- CodingChallenge.Data/Classes/Idioma.cs
+++ CodingChallenge.Data/Classes/Idioma.cs
@@ -1,29 +1,16 @@
 ﻿using CodingChallenge.Data.Classes.Comun;
-using System;
 using System.Text;
 
 namespace CodingChallenge.Data.Classes
 {
     public abstract class Idioma
     {
-        public virtual StringBuilder Cabecera(int formasCount)
-        {
-            throw new ArgumentOutOfRangeException(@"Idioma no soportado");
-        }
+        public abstract StringBuilder Cabecera(int formasCount);
 
-        public virtual string ObtenerLinea(int cantidad, decimal area, decimal perimetro, TipoFiguras tipoFiguras)
-        {
-            throw new ArgumentOutOfRangeException(@"Idioma no soportado");
-        }
+        public abstract string ObtenerLinea(int cantidad, decimal area, decimal perimetro, TipoFiguras tipoFiguras);
 
-        public virtual string TraducirForma(TipoFiguras tipoFiguras, int cantidad)
-        {
-            throw new ArgumentOutOfRangeException(@"Idioma no soportado");
-        }
+        public abstract string TraducirForma(TipoFiguras tipoFiguras, int cantidad);
 
-        public virtual StringBuilder TraducirTotal(int numeroCuadrados, int numeroCirculos, int numeroTriangulos, int numeroTrapecios, decimal areaCuadrados, decimal areaCirculos, decimal areaTriangulos, decimal areaTrapecios, decimal perimetroCuadrados, decimal perimetroCirculos, decimal perimetroTriangulos, decimal perimetroTrapecios)
-        {
-            throw new ArgumentOutOfRangeException(@"Idioma no soportado");
-        }
+        public abstract StringBuilder TraducirTotal(int numeroCuadrados, int numeroCirculos, int numeroTriangulos, int numeroTrapecios, decimal areaCuadrados, decimal areaCirculos, decimal areaTriangulos, decimal areaTrapecios, decimal perimetroCuadrados, decimal perimetroCirculos, decimal perimetroTriangulos, decimal perimetroTrapecios);
     }
 }
